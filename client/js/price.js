70977c49fe24f77e380ee8e7ae752c008bcb3734
HailHydra12138 <97475532+HailHydra12138@users.noreply.github.com>
2025-03-24 12:15:32 +0000

    Update price.js

--- client/js/price.js
+++ client/js/price.js
@@ -37,7 +37,7 @@ module.exports.totalScore = module.exports.totalExperimentScore;
 module.exports.totalEarnings = function (assignment) {
   return assignment.tasks
     .map((task, i) => {
-      return calculateBonus(module.exports.totalTaskScore(i, assignment).totalScore, task);
+      return calculateBonus(module.exports.totalTaskScore(i, assignment), task);
     })
     .reduce(sum, 0);
 };
@@ -80,13 +80,12 @@ module.exports.totalTaskScore = function (taskN, assignment) {
   const actualsArr = assignment.values[taskN];
   const longRunningAveragePredHistArr = assignment.longRunningAveragePredHist[taskN];
   let totalScore = 0;
-
-  // 使用 assignment id 作为随机种子，并选择一个随机回合作为 bonus round
-  const seed = assignment.surveyTime;
-  const random = new SeedRandom(seed);
-  const total_round_idx = task.trainingRounds + task.testingRounds - 1;
-  const bonusRound = random.randomInt(0, total_round_idx);
-
+  // use the assignment id as random seed, and pick a random round to be the bonus round
+  const seed = assignment.surveyTime
+  const random = new SeedRandom(seed),
+      total_round_idx = task.trainingRounds + task.testingRounds -1,
+      bonusRound = random.randomInt(0, total_round_idx);
+  // console.log("bonus round info", assignment, seed, total_round_idx,  bonusRound);
   for (var roundN = 0; roundN < (task.trainingRounds + task.testingRounds); roundN++) {
     if (roundN !== bonusRound) {
       continue;
@@ -94,17 +93,10 @@ module.exports.totalTaskScore = function (taskN, assignment) {
     if (roundN >= task.trainingRounds) {
       const predictions = getPredictions(roundN, predictionsArr, task);
       const actuals = getActuals(roundN, actualsArr, task);
+      totalScore += predictions.map((pred, i) => {
+        return scoreForPrediction(task, pred, actuals[i]);
+      }).reduce(sum, 0);
 
-      if (predictions.length > 1) {
-        // 生成一个随机索引 0 或 1（如果有两个预测值）
-        const randomIndex = new SeedRandom(seed + roundN).randomInt(0, predictions.length - 1);
-        totalScore += scoreForPrediction(task, predictions[randomIndex], actuals[randomIndex]);
-      } else {
-        // 如果只有一个预测值，直接计算
-        totalScore += scoreForPrediction(task, predictions[0], actuals[0]);
-      }
-
-      // 如果任务要求长期预测，则计算长期预测得分
       if (task.predictLongRunning) {
         totalScore += scoreForPrediction(task, longRunningAveragePredHistArr[roundN], 0);
       }
@@ -116,7 +108,7 @@ module.exports.totalTaskScore = function (taskN, assignment) {
 module.exports.totalExperimentScore = function (assignment) {
   let totalScore = 0;
   for (var taskN = 0; taskN < assignment.tasks.length; taskN++) {
-    totalScore += module.exports.totalTaskScore(taskN, assignment).totalScore;
+    totalScore += module.exports.totalTaskScore(taskN, assignment);
   }
   return totalScore;
 };
